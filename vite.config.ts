6ba646bb88b0d8c8b3f5515174e6fabc6488e8cf
programmer_lz <1225186748@qq.com>
2022-10-22 20:44:21 +0800

    fix: fix vueUse auto import

--- vite.config.ts
+++ vite.config.ts
@@ -4,43 +4,41 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // alias
-    resolve: {
-        alias: {
-            '@': '/src',
-        },
+  // alias
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    plugins: [
-        vue(),
-        AutoImport({
-            // targets to transform
-            include: [
-                /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-                /\.vue$/,
-                /\.vue\?vue/, // .vue
-            ],
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+      ],
 
-            // global imports to register
-            imports: [
-                // presets
-                'vue',
-                'vue-router',
-            ],
-            dirs: [
-                './src/hooks',
-            ],
-            dts: './auto-imports.d.ts',
-            vueTemplate: false,
-            resolvers: [
-                /* ... */
-            ],
-            // Generate corresponding .eslintrc-auto-import.json file.
-            // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
-            eslintrc: {
-                enabled: true, // Default `false`
-                filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
-                globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
-            },
-        }),
-    ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+      ],
+      dirs: ['./src/hooks'],
+      dts: './src/auto-imports.d.ts',
+      vueTemplate: false,
+      resolvers: [
+        /* ... */
+      ],
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+  ],
 })
