4ec2189b08022f217627055727c85820aefa63c9
wear工程师 <1225186748@qq.com>
2023-09-05 14:39:18 +0800

    feat: add unocss and base page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,32 +1,16 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo">
-    </a>
-
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
-    </a>
+  <div class="flex w-80vw mx-a mt-40px h-auto sm:h-80vh flex-wrap">
+    <!-- left -->
+    <div class="h-full w-full md:w-1/2 lg:w-2/5">
+      <h3 class="text-2xl font-bold">
+        👋你好
+      </h3>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
